96d73e2770b981a03fe2117eb12a501f9428635c
prathamsharma4 <142142821+prathamsharma4@users.noreply.github.com>
2024-09-12 10:57:29 +0530

    Add files via upload

---
+++ src/main/java/com/automationpractice/listener/RetryFailedTestCases.java
@@ -0,0 +1,33 @@
+package com.automationpractice.listener;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class RetryFailedTestCases implements IRetryAnalyzer {
+
+	private int retryCount = 0;
+	// If there are any failed test cases, then it runs 2 times
+	private int maxRetryCount = 1;
+	
+	@Override
+	public boolean retry(ITestResult arg0) {
+		if(retryCount < maxRetryCount) {
+			
+		}
+		return false;
+	}
+	
+	public void mainteast()
+	{
+		int a = 10;
+		int b = 5;
+		System.out.println(a+b);
+	}
+	public void sum() {
+		int a=1;
+		int b=2;
+		int c=3;
+		System.out.println(a+b+2);
+		}
+
+}
---
+++ src/main/java/com/automationpractice/scripts/MyOwnClass.java
@@ -0,0 +1,9 @@
+package com.automationpractice.scripts;
+
+public class MyOwnClass {
+	
+	String str = "";
+	
+	int a = 10;
+
+}
